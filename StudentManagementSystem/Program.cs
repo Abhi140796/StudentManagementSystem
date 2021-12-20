9857e924bfe9b34694758e3a55022d7e1e39adbb
Abhishek Dubey <abhishekd905@gmail.com>
2021-12-20 22:33:34 +0530

    student mangemnet system class student work pending

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StudentManagementSystem
 {
@@ -6,7 +7,7 @@ namespace StudentManagementSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              
         }
     }
 }
